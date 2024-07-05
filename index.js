d5ecd7abd9a8fe7c0c1e719f8033dd797cc1ad0c
jagedos <jagedosusrecomm@gmail.com>
2024-07-05 13:51:56 +0300

    scope assignment coded

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+function upperCaseCustomerName(){
+customerName = customerName.toUpperCase();
+// console.log(customerName)
+}
+
+// upperCaseCustomerName()
+var bestCustomer;
+function setBestCustomer(){
+
+bestCustomer = 'not bob';
+}
+
+// setBestCustomer()
+
+// console.log(bestCustomer)
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob';
+   
+}
+
+const leastFavoriteCustomer = 'gogo';
+
+function changeLeastFavoriteCustomer (){
+    leastFavoriteCustomer = 'lala';
+    // console.log(leastFavoriteCustomer)
+}
+
+// leastFavoriteCustomer()
